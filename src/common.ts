c0cfe5a13ee250d2fe6124bd6d639e466c4e4a86
ono ono <azazabc123@gmail.com>
2021-09-29 03:56:32 +0800

    Fix wrong meaning in customed MPL-2.0 header

--- src/common.ts
+++ src/common.ts
@@ -1,24 +1,15 @@
 /**
- * MIT License
+ * Copyright (c) 2021 ono ono (kuanyui) All rights reserved.
  *
- * Copyright (c) 2021 ono ono (kuanyui)
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0 (MPL-2.0). If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/.
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * You may not remove or alter the substance of any license notices (including
+ * copyright notices, patent notices, disclaimers of warranty, or limitations of
+ * liability) contained within the Source Code Form of the Covered Software,
+ * except that You may alter any license notices to the extent required to
+ * remedy known factual inaccuracies. (Cited from MPL - 2.0, chapter 3.3)
  */
 
 export type my_msg_t = 'showNotification'
--- src/options_ui/options_ui.ts
+++ src/options_ui/options_ui.ts
@@ -1,21 +1,16 @@
 /**
- * Copyright (c) 2021 ono ono (kuanyui)
- * All rights reserved.
+ * Copyright (c) 2021 ono ono (kuanyui) All rights reserved.
  *
- * This project is released under  Mozilla Public License, v. 2.0 (MPL-2.0). Each
- * file under this directory is licensed under MPL-2.0 by default, if the file
- * includes no license information.
- *
- * If a copy of the MPL was not distributed with this file, You can obtain one at
- * https://mozilla.org/MPL/2.0/.
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0 (MPL-2.0). If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/.
  *
  * You may not remove or alter the substance of any license notices (including
  * copyright notices, patent notices, disclaimers of warranty, or limitations of
- * liability) contained within the Source Code Form of the Covered Software, except
- * that You may alter any license notices to the extent required to remedy known
- * factual inaccuracies. (Cited from MPL - 2.0, chapter 3.3)
+ * liability) contained within the Source Code Form of the Covered Software,
+ * except that You may alter any license notices to the extent required to
+ * remedy known factual inaccuracies. (Cited from MPL - 2.0, chapter 3.3)
  */
-
 
 import { MyStorage, storageManager } from "../common"
 
